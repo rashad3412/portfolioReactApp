21d7f6248f0a8021a7616c64639d5794da40c4ff
Rashad Edwards <rashad3412@users.noreply.github.com>
2025-02-13 21:56:29 -0500

    updated portfolio site

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,21 +11,24 @@ function ProjectPage() {
       {/* Project Section */}
       <div className="w-11/12 bg-neutral-800 border border-stone-800 mx-auto p-6 rounded-lg shadow-md">
         {/* Project Title */}
-        <h3 className="text-2xl font-normal text-beige mb-4">PhiloMood</h3>
+        <h3 className="text-2xl font-normal text-beige mb-4">MeezHaul</h3>
 
         {/* Project Description */}
         <p className="text-stone-300 text-l leading-6 font-extralight">
-          PhiloMood is a single-page React application designed to inspire and
-          guide users by providing random philosophical quotes paired with their
-          authors. Users can interact with the app to get quotes that encourage
-          mindfulness and introspection.
+          MeezHaul is a moving service app that simplifies the process of
+          booking, packing, and relocating. It connects users with professional
+          movers for loading, unloading, and transportation, ensuring a smooth
+          and stress-free move. With an easy-to-use interface, MeezHaul helps
+          customers plan their move efficiently while providing reliable
+          services for both local and long-distance relocations. <br />
+          *On-going Project*
         </p>
 
         {/* Action Buttons */}
         <div className="flex justify-start gap-4 mt-6">
           {/* GitHub Link */}
           <a
-            href="https://github.com/rashad3412/philoMood"
+            href="https://github.com/rashad3412/moveTemplateReact"
             target="_blank"
             rel="noopener noreferrer"
             className="px-4 py-2 bg-beige text-black text-sm font-semibold rounded hover:bg-opacity-90 shadow transition"
@@ -34,7 +37,7 @@ function ProjectPage() {
           </a>
           {/* Live Site Link */}
           <a
-            href="https://philomood.netlify.app/"
+            href="https://movetemplatereact.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
             className="px-4 py-2 bg-beige text-black text-sm font-semibold rounded hover:bg-opacity-90 shadow transition"
